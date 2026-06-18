89f3f350dbaed38079603c4b9633780258a6e9f4
DharshiniSankar <dharshsankar25@gmail.com>
2026-06-18 14:36:26 +0530

    Changes in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import {BrowserRouter , Routes , Route} from 'react-router-dom';
 import Home from './pages/Home';
 import Register from './pages/Register';
-import Login from './pages/login';
+import Login from './pages/Login';
 import Mappage from './pages/Mappage';
 function App(){
   return(
