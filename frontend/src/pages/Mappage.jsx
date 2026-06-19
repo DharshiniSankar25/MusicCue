762f36cac7d0424614953dc0add32fbf7d0c4c72
DharshiniSankar <dharshsankar25@gmail.com>
2026-06-19 10:07:31 +0530

    fixed leaf-let marker

--- frontend/src/pages/Mappage.jsx
+++ frontend/src/pages/Mappage.jsx
@@ -1,7 +1,20 @@
 import {useState,useEffect} from 'react';
 import axios from 'axios';
-import { MapContainer, TileLayer ,Marker,Popup} from "react-leaflet";
+import L from "leaflet";
 
+import "leaflet/dist/leaflet.css";
+
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 function Mappage(){
     const [song,setSong]=useState("");
     const [artist,setArtist]=useState("");
